7df9630fc42201a9a2f70cb3d482e2d2ae4b7950
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-23 15:15:13 +0300

    fix layouts

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -13,8 +13,3 @@
 <li class="{{ Request::is('users*') ? 'active' : '' }}">
     <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
 </li>
-
-<li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
-</li>
-
