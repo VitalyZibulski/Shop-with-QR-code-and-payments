c88f09dc83e61dda7127de876bb1587f990142cd
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-23 14:27:42 +0300

    generate users

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::resource('qrcodes', 'QrcodeController');
 Route::resource('roles', 'RoleController');
 
 Route::resource('transactions', 'TransactionController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('users', 'UserController');
