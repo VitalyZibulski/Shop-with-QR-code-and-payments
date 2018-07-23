0b02bf4cafa4c934b9de0cc0d8f73c112c8ea90f
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-23 12:44:50 +0300

    add roles

--- database/migrations/2018_07_23_092027_create_transactions_table.php
+++ database/migrations/2018_07_23_092027_create_transactions_table.php
@@ -15,6 +15,7 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
