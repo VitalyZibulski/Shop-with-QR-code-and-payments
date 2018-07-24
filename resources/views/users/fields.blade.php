ba34db25cac985768e6b2a590e5771c27636feaf
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-25 01:22:55 +0300

    change edit profile template with options

--- resources/views/users/fields.blade.php
+++ resources/views/users/fields.blade.php
@@ -4,12 +4,23 @@
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Role Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('role_id', 'User level:') !!}
-    {!! Form::number('role_id', null, ['class' => 'form-control']) !!}
-</div>
+@if(Auth::user()->role_id < 3)
+    <!-- Role Id Field -->
+    <div class="form-group col-sm-6">
+        {!! Form::label('role_id', 'User level:') !!}
+        {!! Form::number('role_id', null, ['class' => 'form-control']) !!}
+    </div>
 
+    <div class="form-group col-sm-6">
+        <label for="sel1">User levels:</label>
+        <select class="form-control" id="sel1">
+        <option value="{{$user->role['id']}}}">{{$user->role['name']}}</option>
+            @foreach($roles as $role)
+                <option value="{{$role['id']}}}">{{$role['name']}}</option>
+            @endforeach
+        </select>
+    </div>
+@endif
 <!-- Email Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('email', 'Email:') !!}
