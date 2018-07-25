c02186f5e304b38b826766a1a18f6ee5bf32c6ce
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-25 18:21:14 +0300

    change template accounts

--- resources/views/accounts/table.blade.php
+++ resources/views/accounts/table.blade.php
@@ -1,21 +1,15 @@
 <table class="table table-responsive" id="accounts-table">
     <thead>
         <tr>
-            <th>User Id</th>
+        <th>User Id</th>
         <th>Balance</th>
         <th>Total Credit</th>
         <th>Total Debit</th>
         <th>Withdrawal Method</th>
-        <th>Payment Email</th>
-        <th>Bank Name</th>
-        <th>Bank Branch</th>
-        <th>Bank Account</th>
         <th>Applied For Payout</th>
         <th>Paid</th>
         <th>Last Date Applied</th>
         <th>Last Date Paid</th>
-        <th>Country</th>
-        <th>Other Details</th>
             <th colspan="3">Action</th>
         </tr>
     </thead>
@@ -27,16 +21,10 @@
             <td>{!! $account->total_credit !!}</td>
             <td>{!! $account->total_debit !!}</td>
             <td>{!! $account->withdrawal_method !!}</td>
-            <td>{!! $account->payment_email !!}</td>
-            <td>{!! $account->bank_name !!}</td>
-            <td>{!! $account->bank_branch !!}</td>
-            <td>{!! $account->bank_account !!}</td>
             <td>{!! $account->applied_for_payout !!}</td>
             <td>{!! $account->paid !!}</td>
             <td>{!! $account->last_date_applied !!}</td>
             <td>{!! $account->last_date_paid !!}</td>
-            <td>{!! $account->country !!}</td>
-            <td>{!! $account->other_details !!}</td>
             <td>
                 {!! Form::open(['route' => ['accounts.destroy', $account->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,6 +2,14 @@
     <a href="{!! route('transactions.index') !!}"><i class="fa fa-edit"></i><span>Transactions</span></a>
 </li>
 
+<li class="{{ Request::is('accounts*') ? 'active' : '' }}">
+    <a href="{!! route('accounts.index') !!}"><i class="fa fa-edit"></i><span>Accounts</span></a>
+</li>
+
+<li class="{{ Request::is('accountHistories*') ? 'active' : '' }}">
+    <a href="{!! route('accountHistories.index') !!}"><i class="fa fa-edit"></i><span>Account Histories</span></a>
+</li>
+
 @if(Auth::user()->role_id < 4)
 <li class="{{ Request::is('qrcodes*') ? 'active' : '' }}">
     <a href="{!! route('qrcodes.index') !!}"><i class="fa fa-edit"></i><span>Qrcodes</span></a>
@@ -19,11 +27,5 @@
     <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
 </li>
 @endif
-<li class="{{ Request::is('accounts*') ? 'active' : '' }}">
-    <a href="{!! route('accounts.index') !!}"><i class="fa fa-edit"></i><span>Accounts</span></a>
-</li>
 
-<li class="{{ Request::is('accountHistories*') ? 'active' : '' }}">
-    <a href="{!! route('accountHistories.index') !!}"><i class="fa fa-edit"></i><span>Account Histories</span></a>
-</li>
 
