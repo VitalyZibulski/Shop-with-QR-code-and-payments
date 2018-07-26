f6919f4d1f736a3febbf99d9935a22bda5050233
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-26 08:25:34 +0300

    change acccount template

--- resources/views/accounts/table.blade.php
+++ resources/views/accounts/table.blade.php
@@ -11,18 +11,18 @@
     <tbody>
     @foreach($accounts as $account)
         <tr>
-            <td>{!! $account->user['email'] !!}</td>
+            <td>
+                <a href="{!! route('accounts.show', [$account->id]) !!}" class='btn btn-default btn-xs'>
+                    {!! $account->user['email'] !!}
+                </a>
+            </td>
             <td>${!! number_format($account->balance) !!}</td>
             <td>${!! number_format($account->total_credit) !!}</td>
             <td>${!! number_format($account->total_debit) !!}</td>
             <td>
-                {!! Form::open(['route' => ['accounts.destroy', $account->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('accounts.show', [$account->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
                     <a href="{!! route('accounts.edit', [$account->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
-                {!! Form::close() !!}
             </td>
         </tr>
     @endforeach
